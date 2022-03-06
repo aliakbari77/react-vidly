a66c0aef394342d46283ad7493da81c84d2ed396
Ali Akbari <7731.aa@gmail.com>
2022-03-06 08:13:09 +0330

    feat: add services folder for fakemovies

---
+++ src/components/movies.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    // ...
+  };
+
+  render() {
+    console.log(this.state.movies);
+
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.forEach((m) => {
+              return (
+                <tr key={m._id}>
+                  <td>{m.title}</td>
+                  <td>{m.genre.name}</td>
+                  <td>{m.numberInStock}</td>
+                  <td>{m.dailyRentalRate}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
