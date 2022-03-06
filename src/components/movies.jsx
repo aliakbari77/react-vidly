362e017ef928b1ca227db8097379e38ef4cc6068
Ali Akbari <7731.aa@gmail.com>
2022-03-06 21:45:11 +0330

    test

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,37 +6,59 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
-  handleDelete = (movie) => {
+  handleDelete = (m_id) => {
     // ...
+    console.log(m_id);
+    this.state.movies = this.state.movies.filter((m) => m._id !== m_id);
+    this.setState({ movies: this.state.movies });
+  };
+
+  renderTableData = () => {
+    let list = this.state.movies.map((m) => {
+      return (
+        <tr key={m._id}>
+          <td>{m.title}</td>
+          <td>{m.genre.name}</td>
+          <td>{m.numberInStock}</td>
+          <td>{m.dailyRentalRate}</td>
+          <button
+            onClick={() => {
+              this.handleDelete(m._id);
+            }}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </tr>
+      );
+    });
+    return list;
+  };
+
+  renderTableHeader = () => {
+    return (
+      <tr>
+        <th>Title</th>
+        <th>Genre</th>
+        <th>Stock</th>
+        <th>Rate</th>
+      </tr>
+    );
   };
 
   render() {
     console.log(this.state.movies);
-
+    if (this.state.movies.length == 0)
+      return (
+        <div className="container">
+          <h2>There are no movie</h2>
+        </div>
+      );
     return (
       <div>
         <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Stock</th>
-              <th scope="col">Rate</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.forEach((m) => {
-              return (
-                <tr key={m._id}>
-                  <td>{m.title}</td>
-                  <td>{m.genre.name}</td>
-                  <td>{m.numberInStock}</td>
-                  <td>{m.dailyRentalRate}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <thead>{this.renderTableHeader()}</thead>
+          <tbody>{this.renderTableData()}</tbody>
         </table>
       </div>
     );
