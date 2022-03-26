0c4d818877dde7c9e7a055ceb480f30ee515bdbe
Ali Akbari <7731.aa@gmail.com>
2022-03-26 15:47:16 +0430

    feat: Extracting a Reusable Form, create form.jsx

---
+++ src/components/common/form.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  // this validate is control entire of forms.
+  // when the user is clicked the button we
+  // calling this validate.
+  validate = () => {
+    const options = { abortEarly: false };
+    // the result has an array property like error
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    // the error is an array:
+    // each item of an error has: context, message, path, type.
+    // path[0]: that we set in schema
+    for (let item of error.details) errors[item.path[0]] = item.message;
+
+    return errors;
+  };
+
+  // this validate is control on element of forms.
+  // when change state, we calling this validate.
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+}
+
+export default Form;
